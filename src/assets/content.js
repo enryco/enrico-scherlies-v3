9b55288e888ad5ef276e67a70254d04f510ee488
Enrico Scherlies <e.scherlies@me.com>
2018-04-22 13:17:45 +0200

    content update

--- src/assets/content.js
+++ src/assets/content.js
@@ -2,90 +2,116 @@ const content = {
   entries: [
     {
       title: 'OSCEdays: workshop',
-      type: 'project',
-      date: '2018-04-11T12:00:00',
+      type: 'misc',
+      date: '2018-04-10T12:00:00',
       content: `<div>2018</div>
       <div><a href="https://oscedays.org/">{OSCEdays.org}</a></div>
-      <div>I am giving giving a workshop on 'circular coding':
-      Learn how to use coding as a super power to impact the world and see what current plattforms are already making a change.</div>
+      <div>At the OSCEdays 2018 in Berlin I will give a workshop about: <br/> <strong>Circular Coding</strong> — learn about the roots of open source, how you can use coding as a superpower and let's explore how software can make an positive impact</div>
       `
     },
     {
-    title: 'kibo',
-    type: 'project',
-    date: '2018-03-10T12:00:00',
-    content: `<div>2017 - current</div>
+      title: 'chat for 3000+ people',
+      type: 'project',
+      date: '2018-03-10T12:00:00',
+      content: `<div>2018</div>
+      <div><a href="http://kitabote.de">{kitabote.de}</a></div>
+      <div>plattform: react web app integrated in a wordpress membership site</div>
+      <div>tech: react, firebase</div>
+      <div>A custom chat solution for a client which is scalable (buzzwordalert!), can support at minimum 3000 people chatting in a groupchat (yes, like the twitch or youtube chats)</div>`
+    },
+    {
+      title: 'open tech school-web frontend co-learning co-host',
+      type: 'misc',
+      date: '2018-04-03T12:00:00',
+      content: `<div>2018</div>
+      <div><a href="https://www.meetup.com/opentechschool-berlin/events/">{open tech school meetups}</a></div>
+      <div>im happy to support the open tech school web-frontend co-learning meetup occuring every wednesday in organizational matters</div>`
+    },
+    {
+      title: 'kibo',
+      type: 'project',
+      date: '2018-02-10T12:00:00',
+      content: `<div>2017 - current</div>
       <div><a href="http://kitabote.de">{kitabote.de}</a></div>
       <div>plattform: iOS, Android, Web App</div>
       <div>tech: react, react native, firebase, javascript, html/css</div>
       <div>kitabote is a communications app for child daycares: for parents and daycare managers</div>`
-  },
-  {
-    title: 'ela crain',
-    type: 'project',
-    date: '2018-03-10T12:00:00',
-    content: `<div>2017 - current</div>
+    },
+    {
+      title: 'ela crain',
+      type: 'project',
+      date: '2018-02-10T12:00:00',
+      content: `<div>2017 - current</div>
       <div><a href="http://elacrain.com">{elacrain.com}</a></div>
       <div>plattform: web app</div>
       <div>ela crain's personal website for coaching and writing</div>
       <div>tech: react + wordpress, scss</div>`
-  },
-  {
-    title: 'bonnie & byte',
-    type: 'project',
-    date: '2018-03-10T12:00:00',
-    content: `<div>currently under development</div>
+    },
+    {
+      title: 'bonnie & byte',
+      type: 'project',
+      date: '2018-02-10T12:00:00',
+      content: `<div>currently under development</div>
       <div><a href="http://bonniebyte.com">{bonniebyte.com}</a></div>
       <div>bonny & byte berlin is a web agency with strong skills in wordpress and design</div>
       <div>tech: react & wordpress, scss</div>`,
-  },
-  {
-    title: 'free bugs',
-    type: 'project',
-    date: '2018-03-10T12:00:00',
-    content: `<div>2017</div>
+    },
+    {
+      title: 'free bugs',
+      type: 'project',
+      date: '2018-02-10T12:00:00',
+      content: `<div>2017</div>
       <div><a href="http://enricoscherlies.com/freeBugs">{free bugs}</a></div>
       <div>project for the <a href="https://codingdavinci.de/">{coding da vinci}</a> hackathon</div>
       <div>tech: react</div>`,
-  },
-  {
-    title: 'mh hub',
-    type: 'project',
-    date: '2018-03-10T12:00:00',
-    content: `<div>2017 - present</div>
+    },
+    {
+      title: 'mh hub',
+      type: 'project',
+      date: '2018-02-10T12:00:00',
+      content: `<div>2017 - present</div>
       <div><a href="http://mhhub.org/">{mh hub}</a></div>
       <div>i am building a research charing plattform for them.</div>
       <div>i also consult and maintain the tech at mhhub</div>`,
-  },
-  {
-    title: 'tc',
-    type: 'project',
-    date: '2018-03-10T12:00:00',
-    content: `<div>2017 - present</div>
+    },
+    {
+      title: 'tc',
+      type: 'project',
+      date: '2018-02-10T12:00:00',
+      content: `<div>2017 - present</div>
       <div>to be announced</div>
       <div>plattform: iOS, Android, Web App</div>
       <div>tech: meteor, codova, webdriver, mocha, chimp, digital ocean</div>
       <div>this project is a startup in the health sector — will hit production late this year (2018) so stay tuned</div>`,
-  },
-  {
-    title: 'me',
-    type: 'about',
-    date: '2222-01-01T12:00:00',
-    content: `<div>After studying mechanical engineering i decided to specialize in programming, building software products and becoming an entrepreneur.</div>
-    <div>I love solving complex problems and developing products.</div>`
-  },
-  {
-    title: 'contact',
-    type: 'about',
-    date: '2017-03-22T12:00:00',
-    content: `<div>Enrico Scherlies</div>
+    },
+    {
+      title: 'me',
+      type: 'about',
+      date: '2017-03-23T12:00:00',
+      content: `
+      <div style="display: flex;">
+      <div style="flex: 2">
+      <div>After studying mechanical engineering i decided to specialize in programming, building software products and becoming an entrepreneur.</div>
+      <div>I love solving complex problems and developing products.</div>
+      </div>
+      <div style="flex: 1;">
+      <img style="border-radius: 100%; width:100px; float: right" src="${process.env.PUBLIC_URL}/assets/IMG_1517_xs.jpg" alt=""/>
+      </div>
+      </div>
+    `
+    },
+    {
+      title: 'contact',
+      type: 'about',
+      date: '2017-03-22T12:00:00',
+      content: `<div>Enrico Scherlies</div>
     <div><a href="mailto:contact@enricoscherlies.com">contact@enricoscherlies.com</a></div>
     <div>+49 160 991 666 13</div>
     <div>Schwedenstr. 5</div>
     <div>13357 Berlin</div>
     <div>Germany</div>
     `,
-  }
+    }
   ]
 }
 
